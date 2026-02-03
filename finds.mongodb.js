672c4bd860b9d03a7a4240465afa0aa2d67ce0cb
Jhenie <highhalma@gmail.com>
2026-02-03 08:47:06 -0300

    different types of find

--- finds.mongodb.js
+++ finds.mongodb.js
@@ -1,2 +1,11 @@
+use('bosch')
+db.people.find()
+
 use('bosch');
 db.people.find({ name: "Lays" })
+
+use('bosch');
+db.people.find({ lastName: "joy" })
+
+use('bosch')
+db.people.find( { salary: { $gt: 2105 } } )
