f5eb323347069c9960dc1a22e7d84995a8888e50
Jhenie <highhalma@gmail.com>
2026-02-03 10:42:13 -0300

    using update many

--- updates.mongodb.js
+++ updates.mongodb.js
@@ -1,4 +1,11 @@
 use('bosch')
 db.people.updateOne(
     { _id: ObjectId('6981f2551f82f24350c4f1c9') },
-    { $set: { lastName: "tesoura" } });
+    { $set: { lastName: "tesoura" } }
+);
+
+use('bosch')
+db.people.updateMany(
+    { salary: 2000 },
+    { $set: { salary: 2100 }}
+);
