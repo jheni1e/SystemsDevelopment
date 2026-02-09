03aff3697a6548d51031bca0c1cf5606ee1fda47
Jhenie <highhalma@gmail.com>
2026-02-09 09:15:54 -0300

    using response

--- Aula 03 - API/src/server.ts
+++ Aula 03 - API/src/server.ts
@@ -6,5 +6,5 @@ const port = 8080;
 app.listen(port, () => console.log(`Acesse: http://localhost:${port}/`));
 
 app.get('/', (req, res) => {
-    res.send('GET: Requisição recebida com sucesso!');
+    res.send({ response: 'requisição recebida com sucesso!' });
 });
