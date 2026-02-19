5a98fbc3632a5c80c6fb53e257179bf92f64e280
Jhenie <highhalma@gmail.com>
2026-02-19 10:18:30 -0300

    all http methods added

--- Aula 03 - API/src/routes/person.ts
+++ Aula 03 - API/src/routes/person.ts
@@ -14,20 +14,19 @@ router
         res.status(200).send(people)
     })
     .get('/usuarios/:id', (req: Request, res: Response) => {
-    const { id } = req.params
-    res.status(200).send(`Fazendo um get no servidor! id: ${id}`);
-});
+        const { id } = req.params
+        let convertedId = Number(id)
+        res.status(200).send({ response: people[convertedId] });
+    })
+    .get('/filtro', (req: Request, res: Response) => {
+        const { nome, sobrenome } = req.query
+        res.status(200).send({ response: `${nome} ${sobrenome}` });
+    })
+    .put('/atualizar/:id', (req: Request, res: Response) => {
+        const { id } = req.params;
+        const { nome, sobrenome } = req.body;
 
-router.get('/getData', (req: Request, res: Response) => {
-    const { nome, sobrenome } = req.query
-    res.status(200).send(`Fazendo um GET no servidor! Nome: ${nome}, Idade: ${sobrenome}`);
-});
-
-router.put('/usuarios/:id', (req: Request, res: Response) => {
-    const { id } = req.params;
-    const { nome, sobrenome } = req.body;
-
-    res.status(200).send(`Pessoa com o id: ${id} foi atualizado para ${nome} ${sobrenome}`)
-});
+        res.status(200).send({response: `Atualizando o usuário ${id} -> ${nome} ${sobrenome}`})
+    });
 
 export default router;
