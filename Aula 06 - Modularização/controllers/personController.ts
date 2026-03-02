d576505719b21118cd2ac7a36b164cfef5058431
Jhenie <highhalma@gmail.com>
2026-03-02 09:03:05 -0300

    using controllers in ts

---
+++ Aula 06 - Modularização/controllers/personController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import Person from "../src/models/person.ts";
+
+class PersonController {
+    static async getUsers(req: Request, res: Response) {
+        const users = await Person.find();
+        return res.status(200).send({ response: users });
+    }
+
+    static async registerUser(req: Request, res: Response) {
+        
+    }
+}
+
+export default PersonController;
---
+++ Aula 06 - Modularização/src/routes/person.ts
@@ -0,0 +1,9 @@
+import express, { Router } from 'express';
+
+const router: Router = express.Router();
+
+router
+    .post('/register', )
+    .get('/people', );
+    
+export default router;
